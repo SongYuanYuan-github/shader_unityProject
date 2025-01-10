24eab71743ba764d010ae61082c8d908ae98c4cb
songyuanyuan <syy164@163.com>
2025-01-10 11:47:18 +0800

    [update] init

---
+++ Assets/EventCenter/EventCenter.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum EventType
+{
+    ShowText,
+    Chat,
+    ShowMenu,
+    HideMenu,
+    事故后果分析,
+    风险辨识,
+    ShowTask,
+    HideTask,
+    HAZOP分析,
+    风险管控措施类别,
+    风险管控措施要求,
+    实施跟踪,
+    开始考核,
+    任务完成,
+    NextBtn,
+    当前进度,
+    HAZOP分析完成,
+    HAZOP初始化,
+    布局图状态设置,
+    HAZOP步骤,
+}
+public class EventCenter 
+{
+    private static EventCenter instance;
+    public static EventCenter Instance {
+        get { 
+        if (instance == null)
+            {
+                instance = new EventCenter();
+            }
+            return instance;
+        }
+    }
+
+
+    private static Dictionary<EventType, Delegate> m_EventTable = new Dictionary<EventType, Delegate>();
+
+    public void AddEvent(EventType type,Action action)
+    {
+      
+        if(m_EventTable.ContainsKey(type))
+        {
+            m_EventTable[type] =(Action)m_EventTable[type]+ action;
+        }
+        else
+        {
+            m_EventTable.Add(type, action);
+        }
+
+    }
+
+    public void RomeEvent(EventType type)
+    {
+
+        if (m_EventTable.ContainsKey(type))
+        {
+            m_EventTable.Remove(type);
+        }
+    }
+
+    public void SendEvent(EventType type)
+    {
+        if (m_EventTable.ContainsKey(type))
+        {
+            Action action = (Action)m_EventTable[type];
+            action.Invoke();
+        }
+    }
+}
---
+++ Assets/MyShader/物体靠近溶解/Main.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    /// <summary>
+    /// 材质球
+    /// </summary>
+    public Material mat;
+/// <summary>
+/// 要靠近的物体
+/// </summary>
+    public Transform m_targetObj;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        mat.SetVector("Vector3_B7D1FB9C",m_targetObj.position);
+    }
+}
